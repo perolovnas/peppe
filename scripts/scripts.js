80c794d0730602bc538008204aad7301cdca40b0
Per Olov Näs <perolovnas@gmail.com>
2022-11-07 23:18:21 +0100

    all in place

---
+++ scripts/scripts.js
@@ -0,0 +1,44 @@
+console.log('lets go');
+
+$(function() {
+    
+    // // First animation
+    // setTimeout(function() { 
+    //     $('.one').addClass('active');
+    // }, 500);
+    // setTimeout(function() { 
+    //     $('.one').addClass('no-active');
+    // }, 2000);
+    // setTimeout(function() { 
+    //     $('.one').addClass('scrollDown');
+    // }, 6000);
+    
+    // // Second animation
+    // setTimeout(function() { 
+    //     $('.two').addClass('active');
+    // }, 2500);
+    // setTimeout(function() { 
+    //     $('.two').addClass('no-active');
+    // }, 4000);
+    
+    // // Third animation
+    // setTimeout(function() { 
+    //     $('.third').addClass('active');
+    // }, 4500);
+    // setTimeout(function() { 
+    //     $('.third').addClass('no-active');
+    // }, 6000);
+
+    // setTimeout(function() { 
+    //     $('.case-wrapper').addClass('active');
+    // }, 2000);
+
+    // setTimeout(function() { 
+    //     $('.top-wrapper svg').addClass('active');
+    // }, 1500);
+
+    // setTimeout(function() { 
+    //     $('.top-wrapper').addClass('active');
+    // }, 1000);
+
+});

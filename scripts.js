cb58326474a77bf2c44d4f52b09fc21136e64e38
uxwebb <perolov.nas@oas.nu>
2024-11-10 18:44:05 +0100

    new version

---
+++ scripts.js
@@ -0,0 +1,16 @@
+console.log('hi');
+
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene, {
+  relativeInput: true
+});
+
+var scene = document.getElementById('scene2');
+var parallaxInstance = new Parallax(scene, {
+  relativeInput: true
+});
+
+var scene = document.getElementById('scene3');
+var parallaxInstance = new Parallax(scene, {
+  relativeInput: true
+});
